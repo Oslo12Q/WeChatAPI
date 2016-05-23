3e92ec45412f3c6795512a9ddc1bca13fa167391
lzq941129 <577142965@qq.com>
2016-05-23 19:03:27 +0800

    Update web.py

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-
-# -*- coding: utf-8 -*-
+#-*- coding: UTF-8 -*- 
+#coding=utf-8
 import sys, os
 import tornado.ioloop
 import tornado.web
@@ -10,9 +10,6 @@ import re
 from urllib import unquote
 
 import xml.etree.ElementTree as ET
-
-
-
 import config
 import pdb
 import hashlib
@@ -20,117 +17,147 @@ import time
 import urllib2
 import json
 import web
-
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 def deamon(chdir = False):
-	try:
-		if os.fork() > 0:
-			os._exit(0)
-	except OSError, e:
-		print 'fork #1 failed: %d (%s)' % (e.errno, e.strerror)
-		os._exit(1)
+        try:
+                if os.fork() > 0:
+                        os._exit(0)
+        except OSError, e:
+                print 'fork #1 failed: %d (%s)' % (e.errno, e.strerror)
+                os._exit(1)
 
 def init():
-	pass
+        pass
 
 class DefaultHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write('WeChatAPI Say Hello!')
+        def get(self):
+                self.write('WeChatAPI Say Hello!')
 
 class TestHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write('WeChatAPI Test!')
+        def get(self):
+                self.write('WeChatAPI Test!')
 
 class LogHandler(tornado.web.RequestHandler):
-	def get(self):
-		log_filename = 'logs/logging'
-		if not os.path.exists(log_filename):
-			self.write('The log file is empty.')
-			return
-		log_file = None
-		log_file_lines = None
-		try:
-			log_file = open(log_filename, 'r')
-			if log_file is None:
-				raise Exception('log_file is None')
-			log_file_lines = log_file.readlines()
-			if log_file_lines is None:
-				raise Exception('log_file_lines is None')
-		except Exception, e:
-			logger = logging.getLogger('web')
-			logger.error('Failed to read the log file (logs/logging), error: %s' % e)
-		finally:
-			if log_file is not None:
-				log_file.close()
-		if log_file_lines is None:
-			self.write('Failed to read the log file.')
-		line_limit = 500
-		for _ in log_file_lines[::-1]:
-			line_limit -= 1
-			if line_limit > 0:
-				self.write(unquote(_) + '<BR/>')
+        def get(self):
+                log_filename = 'logs/logging'
+                if not os.path.exists(log_filename):
+                        self.write('The log file is empty.')
+                        return
+                log_file = None
+                log_file_lines = None
+                try:
+                        log_file = open(log_filename, 'r')
+                        if log_file is None:
+                                raise Exception('log_file is None')
+                        log_file_lines = log_file.readlines()
+                        if log_file_lines is None:
+                                raise Exception('log_file_lines is None')
+                except Exception, e:
+                        logger = logging.getLogger('web')
+                        logger.error('Failed to read the log file (logs/logging), error: %s' % e)
+                finally:
+                        if log_file is not None:
+                                log_file.close()
+                if log_file_lines is None:
+                        self.write('Failed to read the log file.')
+                line_limit = 500
+                for _ in log_file_lines[::-1]:
+                        line_limit -= 1
+                        if line_limit > 0:
+                                self.write(unquote(_) + '<BR/>')
 
 
-	
-	
+
+#创建类 
 class weixin(tornado.web.RequestHandler):
 
-	def get(self):
-		signature = self.get_argument('signature','')
-		timestamp = self.get_argument('timestamp','')
-        	nonce = self.get_argument('nonce','')
-        	echostr = self.get_argument('echostr','')
-		
-		token="liuzhiqiang"
-		list=[token,timestamp,nonce]
-        	list.sort()
-		sha1=hashlib.sha1()
-       	 	map(sha1.update,list)
-		hashcode=sha1.hexdigest()
-        	if hashcode == signature:
-            		self.write(echostr)
-        	else:
- 
-           		self.write('shibai 4055555')
-	
-	def parse_request_xml(self,rootElem):
-        	msg = {}
-        	if rootElem.tag == 'xml':
-            		for child in rootElem:
-				msg[child.tag] = child.text
-		return msg
-	def post(self):
+        def get(self):
+                signature = self.get_argument('signature','')
+                timestamp = self.get_argument('timestamp','')
+                nonce = self.get_argument('nonce','')
+                echostr = self.get_argument('echostr','')
+                #验证密钥
+                token="liuzhiqiang"
+                list=[token,timestamp,nonce]
+                list.sort()
+                #sha1排序
+                sha1=hashlib.sha1()
+                map(sha1.update,list)
+                hashcode=sha1.hexdigest()
+                if hashcode == signature:
+                        self.write(echostr)
+                else:
+
+                        self.write('shibai 4055555')
+
+        def parse_request_xml(self,rootElem):
+                msg = {}
+                if rootElem.tag == 'xml':
+                        for child in rootElem:
+                                msg[child.tag] = child.text
+                return msg
+        #消息回复
+        def post(self):
                 body = self.request.body
                 msg = self.parse_request_xml(ET.fromstring(body))
                 MsgType = tornado.escape.utf8(msg.get("MsgType"))
-        	Content= tornado.escape.utf8(msg.get("Content"))
-        	FromUserName = tornado.escape.utf8(msg.get("FromUserName"))
-        	CreateTime = tornado.escape.utf8(msg.get("CreateTime"))
-        	ToUserName = tornado.escape.utf8(msg.get("ToUserName"))
-        	if MsgType == "event":
-			Content="44444444444444"
-		
-		if MsgType == "text":
-			Content=u"rrrrrrrrrrrrrrrrrrr中文".encoding('utf-8')
-		if MsgType == "image":
-			Content="tttttttttttttttttttt"
-		if MsgType == "voice":
-			Content="yyyyyyyyyyyyyyyyyyyyyyy"
-		if MsgType == "video":
+                Content= tornado.escape.utf8(msg.get("Content"))
+                FromUserName = tornado.escape.utf8(msg.get("FromUserName"))
+                CreateTime = tornado.escape.utf8(msg.get("CreateTime"))
+                ToUserName = tornado.escape.utf8(msg.get("ToUserName"))
+                #关注 click事件
+                if MsgType == 'event':
+                        eventKey = tornado.escape.utf8(msg.get("EventKey"))
+                        sub = tornado.escape.utf8(msg.get("Event"))
+                        #扫码事件，返回文本消息 感谢您的关注，谢谢！
+                        if sub == 'subscribe':
 
-			Content="pppppppppppppppppp"
-		if MsgType == "shortvideo":
-			Content="xxxxxxxxxxxxxxxxxxx"
-		if MsgType == "location":
+                                Content="感谢您的关注，谢谢！"
+                        #click事件
 
-			Content="wwwwwwwwwwwwwwwwwwwwwww"
-		if MsgType =="link":
-			Content="llllllllllllllllllllll"
+                        if eventKey == 'Text_1':
 
-		if not Content:
-			Content="4333333333333333"
+                                dom = xml.dom.minidom.parse('weixin/Text_1.xml')
+                                print dom
+                                Content = "click事件正在处理"
+                if MsgType == 'text':
+                        Content= tornado.escape.utf8(msg.get("Content"))
 
+                        if Content == '1':
 
+                                Content = "数字啊"
+                        else:
+
+                                Content ="000"
+
+                        Content ="您发送的文本消息已收到，谢谢！"
+
+                #图片信息
+                if MsgType == 'image':
+
+                        Content = "您发送的图片信息我们已收到，谢谢！"
+
+                #语音消息
+                if MsgType == 'voice':
+                        Content = "您发送的语音信息我们已收到，谢谢！"
+
+                #视频消息
+                if MsgType == 'video':
+                        Content = "您发送的视频信息我们已收到，谢谢！"
+
+                #小视频信息
+                if MsgType == 'shortvideo':
+                        Content = "您发送的小视频信息我们已经收到，谢谢！"
+
+                #地理位置信息
+                if MsgType == 'location':
+                        Content = "您发的地理位置我们已经收到，谢谢！"
+
+                # url 信息
+                if MsgType == 'link':
+                        Content = "您发送的url链接我们已收到，谢谢！"
                 data = """<xml>
                         <ToUserName><![CDATA[%s]]></ToUserName>
                         <FromUserName><![CDATA[%s]]></FromUserName>
@@ -138,12 +165,12 @@ class weixin(tornado.web.RequestHandler):
                         <MsgType><![CDATA[%s]]></MsgType>
                         <Content><![CDATA[%s]]></Content>
                 </xml>""" % (FromUserName,ToUserName,int(time.time()),'text',Content)
-		self.write(data)
-	
+                self.write(data)
 
 
-		
-	def token(requset):
+
+        #验证密钥
+        def token(requset):
                 pdb.set_trace()
                 url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (
                 Config.AppID, Config.AppSecret)
@@ -151,34 +178,61 @@ class weixin(tornado.web.RequestHandler):
                 Config.access_token = json.loads(result).get('access_token')
                 print 'access_token===%s' % Config.access_token
                 return HttpResponse(result)
-
-	def createMenu(request):
+        #创建菜单
+        def createMenu(request):
                 url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % Config.access_token
                 data = {
-                        "button":[
+                        "button": [
+                        {
+                                "name": "我的信息",
+                                "sub_button": [
                                 {
-                                        "type":"click",
-                                        "name":"111",
-                                        "key":"100000010"
+                                        "type": "click",
+                                        "name":"商品中心",
+                                        "key": "meitu"
                                 },
                                 {
-                                        "name":"000",
-                                        "sub_button":[
-                                        {
-                                                "type":"view",
-                                                "name":"222",
-                                                "url":"http://user.qzone.qq.com/577142965"
-                                        },
-                                        {
-                                                "type":"view",
-                                                "name":"444",
-                                                "url":"http://www.baidu.com/"
-                                        }]
-                                }
-                        ]
+                                        "type": "view",
+                                        "name": "我的订单",
+                                        "url": "http://m.jb51.net/photos"
+                                },
+                                {
+                                        "type": "view",
+                                        "name": "精彩回顾",
+                                        "url": "http://m.qzone.com/infocenter?g_f=#2378686916/mine"
+                                }]
+                        },
+                        {
+                                "name": "精彩世界",
+                                "sub_button": [
+                                {
+                                        "type": "view",
+                                        "name": "1",
+                                        "url":"http://jb51.net/zhuanti/freedesign.jsp?src=3"
+                                },
+                                {
+                                        "type": "view",
+                                        "name": "2",
+                                        "url": "http://jb51.net/zhuanti/freedesign.jsp?src=3"
+                                },
+                                {
+                                        "type": "view",
+                                        "name": "3",
+                                        "url": "http://jb51.net/zhuanti/freedesign.jsp?src=3"
+                                },
+                                {
+                                        "type": "view",
+                                        "name": "4",
+                                        "url": "http://m.jb51.net/projects"
+                                }]
+                        },
+                        {
+                                "type": "view",
+                                "name": "百度一下",
+                                "url": "http://www.baidu.com/"
+                        }]
                 }
-		
-		
+
                 req = urllib2.Request(url)
                 req.add_header('Content-Type', 'application/json')
                 req.add_header('encoding', 'utf-8')
@@ -186,44 +240,43 @@ class weixin(tornado.web.RequestHandler):
                 result = response.read()
                 return HttpResponse(result)
 
-		
+
 settings = {
-	"static_path": os.path.join(os.path.dirname(__file__), "static"),
+        "static_path": os.path.join(os.path.dirname(__file__), "static"),
 }
 
 routes = [
-	(r"/", DefaultHandler),
-	(r"/wx/test", TestHandler),
-	(r"/wx/weixin", weixin),
+        (r"/", DefaultHandler),
+        (r"/wx/test", TestHandler),
+        (r"/wx/weixin", weixin),
 ]
-
 if config.Mode == 'DEBUG':
-	routes.append((r"/log", LogHandler))
+        routes.append((r"/log", LogHandler))
 
 application = tornado.web.Application(routes, **settings)
 
 if __name__ == "__main__":
-	if '-d' in sys.argv:
-		deamon()
-	logdir = 'logs'
-	if not os.path.exists(logdir):
-		os.makedirs(logdir)
-	fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
-	formatter = logging.Formatter(fmt)
-	handler = logging.handlers.TimedRotatingFileHandler(
-		'%s/logging' % logdir, 'M', 20, 360)
-	handler.suffix = '%Y%m%d%H%M%S.log'
-	handler.extMatch = re.compile(r'^\d{4}\d{2}\d{2}\d{2}\d{2}\d{2}')
-	handler.setFormatter(formatter)
-	logger = logging.getLogger('web')
-	logger.addHandler(handler)
-	if config.Mode == 'DEBUG':
-		logger.setLevel(logging.DEBUG)
-	else:
-		logger.setLevel(logging.ERROR)
+        if '-d' in sys.argv:
+                deamon()
+        logdir = 'logs'
+        if not os.path.exists(logdir):
+                os.makedirs(logdir)
+        fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+        formatter = logging.Formatter(fmt)
+        handler = logging.handlers.TimedRotatingFileHandler(
+                '%s/logging' % logdir, 'M', 20, 360)
+        handler.suffix = '%Y%m%d%H%M%S.log'
+        handler.extMatch = re.compile(r'^\d{4}\d{2}\d{2}\d{2}\d{2}\d{2}')
+        handler.setFormatter(formatter)
+        logger = logging.getLogger('web')
+        logger.addHandler(handler)
+        if config.Mode == 'DEBUG':
+                logger.setLevel(logging.DEBUG)
+        else:
+                logger.setLevel(logging.ERROR)
 
-	init()
+        init()
 
-	application.listen(80)
-	print 'Server is running, listening on port 80....'
-	tornado.ioloop.IOLoop.instance().start()
+        application.listen(80)
+        print 'Server is running, listening on port 80....'
+        tornado.ioloop.IOLoop.instance().start()
